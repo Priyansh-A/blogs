78334063d924f2c18bf28e8c84a1b2271d16cea0
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-07-27 09:37:46 +0545

    added the share feature

--- blog/views.py
+++ blog/views.py
@@ -53,6 +53,24 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f" recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email= None,
+                recipient_list=[cd['to']]
+            )
+            sent=True
     else:
         form = EmailPostForm()
     return render(
@@ -60,6 +78,7 @@ def post_share(request, post_id):
             "blog/post/share.html",
             {
                 'post': post,
-                'form': form
+                'form': form,
+                'sent': sent,
             }
         )    
