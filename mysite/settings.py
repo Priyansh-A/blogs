da947f15f1a2f322b50e475cc2107abbfbe198b4
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-08-04 16:31:01 +0545

    one more time

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,14 +96,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default':  {
         'default': "dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)",
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-}
 }
 
 if getenv('DATABASE_URL'):
