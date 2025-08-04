6c744bea0ef7fb9e43089447324bdb47f10d0c88
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-08-04 17:58:19 +0545

    lets see now ig

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,8 +96,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+    }
+}
+
+DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_PUBLIC_URL'),
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
